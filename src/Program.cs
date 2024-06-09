26a0b150eb1e2bc83ea46bc76eb0c2cfc5d0cf53
David Spörri <davepermen@hotmail.com>
2024-06-10 00:35:10 +0200

    ok

--- src/Program.cs
+++ src/Program.cs
@@ -1,50 +1,9 @@
-using Conesoft.Files;
+using Conesoft.Services.RefreshCertificates;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using System;
 
-namespace Conesoft.Services.RefreshCertificates
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                var log = Hosting.Host.Root / Filename.From(Hosting.Host.Name.ToLowerInvariant() + " log", "txt");
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-                System.IO.File.WriteAllText(log.Path, "");
-
-                Log.Logger = new LoggerConfiguration()
-                    .WriteTo.File(log.Path, buffered: false, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(1))
-                    .CreateLogger();
-            }
-            catch (Exception)
-            {
-                Log.Logger = new LoggerConfiguration()
-                    .WriteTo.Console()
-                    .CreateLogger();
-            }
-
-            Log.Information("App has started");
-
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            finally
-            {
-                Log.Information("App has stopped");
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                })
-                .UseSerilog();
-    }
-}
+var host = builder.Build();
+host.Run();
--- src/Worker.cs
+++ src/Worker.cs
@@ -12,17 +12,9 @@ using System.Threading.Tasks;
 
 namespace Conesoft.Services.RefreshCertificates
 {
-    public class Worker : BackgroundService
+    public class Worker(ILogger<Worker> logger) : BackgroundService
     {
-        private readonly ILogger<Worker> logger;
-        private readonly IConfigurationRoot configuration;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            this.logger = logger;
-            configuration = new ConfigurationBuilder().AddJsonFile(Hosting.Host.GlobalConfiguration.Path).Build();
-        }
-
+        private readonly IConfigurationRoot configuration = new ConfigurationBuilder().AddJsonFile(Hosting.Host.GlobalConfiguration.Path).Build();
 
         X509Certificate2 Load(File file) => new(file.Path, configuration["hosting:certificate-password"]);
 
@@ -33,35 +25,36 @@ namespace Conesoft.Services.RefreshCertificates
 
             await Notify($"Certificate watcher started");
 
-            await foreach (var files in deploymentSource.Live().Changes())
+            var lastUpdate = DateTime.MinValue;
+
+            await foreach (var files in deploymentSource.Live().Changes().WithCancellation(stoppingToken))
             {
-                if (stoppingToken.IsCancellationRequested)
+                if (files.ThereAreChanges || lastUpdate + TimeSpan.FromHours(1) < DateTime.UtcNow)
                 {
-                    return;
-                }
+                    var active = files.All.Files().Select(f => f.NameWithoutExtension).ToArray();
+                    var inactive = certificateStorage.Files.Where(f => active.Contains(f.NameWithoutExtension) == false).ToArray();
 
-                var active = files.All.Select(f => f.NameWithoutExtension).ToArray();
-                var inactive = certificateStorage.Files.Where(f => active.Contains(f.NameWithoutExtension) == false).ToArray();
-
-                foreach (var cert in active)
-                {
-                    var file = certificateStorage / Filename.From(cert, "pfx");
-                    if (file.Exists && Load(file).NotAfter > DateTime.Today + TimeSpan.FromDays(2))
+                    foreach (var cert in active)
                     {
-                        logger.LogInformation("certificate for {cert} is active and up to date (till {date})", cert, Load(file).NotAfter);
+                        var file = certificateStorage / Filename.From(cert, "pfx");
+                        if (file.Exists && Load(file).NotAfter > DateTime.Today + TimeSpan.FromDays(2))
+                        {
+                            logger.LogInformation("certificate for {cert} is active and up to date (till {date})", cert, Load(file).NotAfter);
+                        }
+                        else
+                        {
+                            await Notify($"Updating Certificate for {cert}");
+                            logger.LogInformation("creating certificate for {cert}", cert);
+                            await CreateCertificateFor(file);
+                            logger.LogInformation("... done, valid till {date}", Load(file).NotAfter);
+                        }
                     }
-                    else
+                    foreach (var cert in inactive)
                     {
-                        await Notify($"Updating Certificate for {cert}");
-                        logger.LogInformation("creating certificate for {cert}", cert);
-                        await CreateCertificateFor(file);
-                        logger.LogInformation("... done, valid till {date}", Load(file).NotAfter);
+                        // delete old certificates
+                        logger.LogInformation("found old certificate for {cert}", cert);
                     }
-                }
-                foreach (var cert in inactive)
-                {
-                    // delete old certificates
-                    logger.LogInformation("found old certificate for {cert}", cert);
+                    lastUpdate = DateTime.UtcNow;
                 }
             }
         }
@@ -88,7 +81,7 @@ namespace Conesoft.Services.RefreshCertificates
             await cert.WriteBytes(bytes);
         }
 
-        async Task Notify(string message)
+        private static async Task Notify(string message)
         {
             var configuration = new ConfigurationBuilder().AddJsonFile(Hosting.Host.GlobalSettings.Path).Build();
             var conesoftSecret = configuration["conesoft:secret"] ?? throw new Exception("Conesoft Secret not found in Configuration");

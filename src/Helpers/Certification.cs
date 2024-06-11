006ef6d395b40fb8f3711c51bba9dbde7cb2bd7e
David Spörri <davepermen@hotmail.com>
2024-06-11 05:13:21 +0200

    nice

---
+++ src/Helpers/Certification.cs
@@ -0,0 +1,35 @@
+﻿using Conesoft.Files;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Conesoft.Services.RefreshCertificates.Helpers;
+
+public static class Certification
+{
+    public static async Task CreateCertificate(this File cert, IConfiguration configuration)
+    {
+        var letsEncryptMail = configuration["hosting:letsencrypt-mail"];
+        var dnsimpleToken = configuration["hosting:dnsimple-token"];
+        var certificatePassword = configuration["hosting:certificate-password"];
+
+        var certificateStorage = Hosting.Host.GlobalStorage / "Certificates";
+
+        var client = await LetsEncrypt.Client.Login(letsEncryptMail, () => new HttpClient(), dnsimpleToken, production: true);
+
+        var bytes = await client.CreateCertificateFor(cert.NameWithoutExtension, certificatePassword, new LetsEncrypt.CertificateInformation()
+        {
+            CountryName = "CH",
+            State = "Switzerland",
+            Locality = "Burg AG",
+            Organization = "Conesoft",
+            OrganizationUnit = "Dev"
+        });
+
+        await cert.WriteBytes(bytes);
+    }
+
+    public static X509Certificate2 LoadCertificate(this File file, IConfiguration configuration) => new(file.Path, configuration["hosting:certificate-password"]);
+
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,68 @@
-using Conesoft.Services.RefreshCertificates;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Conesoft.Files;
+using Conesoft.Hosting;
+using Conesoft.Services.RefreshCertificates.Helpers;
+using Humanizer;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Linq;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args);
+builder.Services
+    .AddLoggingToHost()
+    .AddPeriodicGarbageCollection(TimeSpan.FromMinutes(5));
 
 var host = builder.Build();
-host.Run();
+
+await host.StartAsync();
+
+var configuration = new ConfigurationBuilder().AddJsonFile(Host.GlobalConfiguration.Path).Build();
+
+var certificateStorage = Host.GlobalStorage / "Certificates";
+var deploymentSource = Host.Root / "Deployments" / "Websites";
+
+Log.Information("certification watcher started");
+
+var lastUpdate = DateTime.MinValue;
+
+await foreach (var files in deploymentSource.Live().Changes())
+{
+    if (files.ThereAreChanges || lastUpdate + TimeSpan.FromHours(1) < DateTime.UtcNow)
+    {
+        var active = files.All.Files().Select(f => f.NameWithoutExtension).ToArray();
+        var inactive = certificateStorage.Files.Where(f => active.Contains(f.NameWithoutExtension) == false).Select(f => f.NameWithoutExtension).ToArray();
+
+        foreach (var cert in active)
+        {
+            var file = certificateStorage / Filename.From(cert, "pfx");
+            if (file.Exists && file.LoadCertificate(configuration).NotAfter > DateTime.Today + TimeSpan.FromDays(2))
+            {
+                var timespan = file.LoadCertificate(configuration).NotAfter - DateTime.UtcNow;
+                Log.Information("certificate for {cert} is active and up to date (for {date})", cert, timespan.Humanize(precision: 2));
+            }
+            else
+            {
+                await Conesoft.Helpers.Notification.Notify($"Updating Certificate for {cert}");
+                Log.Information("creating certificate for {cert}", cert);
+                await file.CreateCertificate(configuration);
+                Log.Information("... done, valid till {date}", file.LoadCertificate(configuration).NotAfter.Humanize());
+            }
+        }
+
+        foreach (var cert in inactive)
+        {
+            var file = certificateStorage / Filename.From(cert, "pfx");
+            if (file.LoadCertificate(configuration).NotAfter > DateTime.Today)
+            {
+                Log.Information("deleted old certificate for {cert}", cert);
+                file.Delete();
+            }
+            else
+            {
+                Log.Information("found old certificate for {cert}", cert);
+            }
+        }
+
+        lastUpdate = DateTime.UtcNow;
+    }
+}
